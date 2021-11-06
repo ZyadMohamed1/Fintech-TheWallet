9dc56e7d043523e5db48c4edaac86fdcf94424b2
ZyadMohamed1 <zyad344@gmail.com>
2021-11-06 11:36:01 +0200

    security

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6!3f#2%fj0g8mp00!g!i=(cbod30(987m2hljk0t^x1q08-_5v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ django_project/scraping_websites/Search_mode.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+#read from json file
+items = json
